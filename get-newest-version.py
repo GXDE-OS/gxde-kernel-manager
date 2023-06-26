3855e4ef672b730ed2ce009e410d7f2122365c93
gfdgd_xi <3025613752@qq.com>
2023-06-26 20:49:56 +0800

    new yml

--- get-newest-version.py
+++ get-newest-version.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python3
-import os
 import sys
 import pyquery
-
-def CompareVersion(old, new):
-    oldList = old.split(".")
-    newList = new.split(".")
-    for i in range(len(oldList)):
-        oldN = int(oldList[i])
-        try:
-            newN = int(newList[i])
-        except:
-            newN = 0
-        if newN > oldN:
-            return 1
-        if newN < oldN:
-            return -1
-    return 0
-
-version=sys.argv[1]
+mainVersion = int(sys.argv[1])
+programVersionList = pyquery.PyQuery(url=f"https://www.kernel.org/")
+#release = pyquery.PyQuery()
+temp = 0
 newestVersion = "0.0.0"
 newestUrl = ""
-programVersionList = pyquery.PyQuery(url=f"https://cdn.kernel.org/pub/linux/kernel/v{version}.x/")
-for i in programVersionList("a").items():
-    #continue
-    url = i.attr.href
-    if url[-7:] == ".tar.xz" and url[:6] == "linux-":
-        nowversion = os.path.basename(url).lower().replace("linux-", "").replace(".tar.xz", "")
-        if "-" in nowversion:
-            nowversion = nowversion[:nowversion.index("-")]
-        print(newestUrl)
-        if CompareVersion(newestVersion, nowversion) == 1:
-            newestVersion = nowversion
-            newestUrl = url
+for i in programVersionList(f"#releases tr :nth-child(2)").items():
+    version = i("td strong").text()
+    if temp == mainVersion:
+        #print(version)
+        newestVersion = version
+        break
+    temp += 1
+
+temp = 0
+for i in programVersionList(f"#releases tr :nth-child(4)").items():
+    url = i("td a").attr.href
+    if temp == mainVersion:
+        newestUrl = url
+        break
+    temp += 1
 print(newestVersion)
-newestUrl = f"https://cdn.kernel.org/pub/linux/kernel/v{version}.x/{newestUrl}"
+
 print(newestUrl)
 with open("/tmp/kernelversion.txt", "w") as file:
     file.write(newestVersion)
