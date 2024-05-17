69d7617e83c73c09fa13084aa7c6309569e13c5a
gfdgd_xi <3025613752@qq.com>
2024-05-17 22:57:31 +0800

    尝试arm交叉编译

--- build_action-aarch64.sh
+++ build_action-aarch64.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 sudo apt update
-sudo apt install python3-pyquery -y
-python3 get-newest-version.py $1
+sudo apt install gpg python3-pyquery -y
 aria2c $KEY
 gpg --import  --pinentry-mode loopback --batch --passphrase "$KEYPASSWORD"  private-file.key
+python3 get-newest-version.py $1
 #VERSION=$(grep 'Kernel Configuration' < config | awk '{print $3}')
 # add deb-src to sources.list
 VERSION=`cat /tmp/kernelversion.txt`
 URL=`cat /tmp/kernelurl.txt`
 MAINVERSION=`expr substr $VERSION 1 1`
-SHOWVERSION=$VERSION-arm64
-curl https://github.com/gfdgd-xi/gxde-linux-kernel/raw/main/l/linux-headers-$VERSION-arm64-gfdgdxi-desktop/lock | grep 404
-result=$?
+SHOWVERSION=$VERSION
+# 使用 deepin hwe config编译
+if [[ $2 == 1 ]]; then
+    SHOWVERSION=$VERSION-hwe
+fi
+if [[ $2 == 1 ]]; then
+    curl https://github.com/gfdgd-xi/gxde-linux-kernel/raw/main/l/linux-kernel-gxde-gfdgdxi-$1-hwe/linux-kernel-gxde-gfdgdxi-$1-hwe_${VERSION}_arm64.deb | grep 404
+    result=$?
+else
+    curl https://github.com/gfdgd-xi/gxde-linux-kernel/raw/main/l/linux-kernel-gxde-gfdgdxi-$1/linux-kernel-gxde-gfdgdxi-${1}_${VERSION}_arm64.deb | grep 404
+    result=$?
+fi
 if [[ $result != 0 ]]; then
     exit
 fi
 # install dep
 sudo apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev
+sudo apt install -y gcc-aarch64-linux-gnu
 sudo apt build-dep -y linux
-wget http://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
-tar -xvf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
-sudo mkdir /usr/bin/toolchain
-sudo cp gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu /usr/bin/toolchain/ -rfv
-export PATH=$PATH:/usr/bin/toolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/
-echo $PATH
+
 # change dir to workplace
 cd "${GITHUB_WORKSPACE}" || exit
 
@@ -45,7 +50,16 @@ fi
 cd linux-"$VERSION" || exit
 
 # copy config file
-cp ../config-arm64 .config
+# 使用 deepin hwe config编译
+if [[ $2 == 1 ]]; then
+    cp ../config-6.1.11-arm64-desktop-hwe .config
+else
+    cp ../config-arm64 .config
+fi
+echo $VERSION | grep 4.14
+if [[ $? == 0 ]]; then
+    cp ../config-4.19.0-10-arm64 .config
+fi
 #
 # disable DEBUG_INFO to speedup build
 # scripts/config --disable DEBUG_INFO 
@@ -56,29 +70,104 @@ scripts/config --undefine DEBUG_INFO_COMPRESSED
 scripts/config --undefine DEBUG_INFO_REDUCED
 scripts/config --undefine DEBUG_INFO_SPLIT
 scripts/config --undefine GDB_SCRIPTS
+scripts/config --disable DEBUG_INFO
 scripts/config --set-val  DEBUG_INFO_DWARF5     n
 scripts/config --set-val  DEBUG_INFO_NONE       y
 
 # build deb packages
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
-sudo env DEBEMAIL="gfdgd xi <3025613752@qq.com>" make bindeb-pkg ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j"$CPU_CORES"
+sudo env DEBEMAIL="gfdgd xi <3025613752@qq.com>" make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg -j"$CPU_CORES"
+# 移动 deb
+mkdir /tmp/deb-result
+cp ../*.deb /tmp/deb-result
+rm /tmp/deb-result/linux-libc-dev*.deb
+rm /tmp/deb-result/*dbg*.deb
 
-# move deb packages to artifact dir
-cd ..
-mkdir "artifact"
-#cp ./*.deb artifact/
-git clone https://gfdgd-xi:$PASSWORD@github.com/gfdgd-xi/gxde-linux-kernel
-#cd dclc-kernel
-mkdir dclc-kernel
-mkdir dclc-kernel/$VERSION-aarch64
-mkdir dclc-kernel/head
-rm -rfv *dbg*.deb
-mv ./*.deb dclc-kernel/$VERSION-aarch64
-cd dclc-kernel/$VERSION-aarch64
-cd ..
-cd head
-mkdir deb/DEBIAN -vp
-cat > deb-/DEBIAN/control <<EOF
+if [[ $2 == 1 ]]; then
+    # move deb packages to artifact dir
+    cd ..
+    mkdir "artifact"
+    #cp ./*.deb artifact/
+    rm -rfv "${GITHUB_WORKSPACE}/linux-$VERSION*"
+    git clone https://gfdgd-xi:$PASSWORD@github.com/gfdgd-xi/gxde-linux-kernel --depth=1
+    mkdir dclc-kernel
+    mkdir dclc-kernel/$SHOWVERSION
+    mkdir dclc-kernel/head
+    rm -rfv *dbg*.deb
+    mv ./*.deb dclc-kernel/$SHOWVERSION
+    cd dclc-kernel/$SHOWVERSION
+    cd ..
+    cd head
+    mkdir deb/DEBIAN -vp
+    cat > deb/DEBIAN/control <<EOF
+Package: linux-kernel-gxde-gfdgdxi-hwe
+Version: $VERSION
+Maintainer: gfdgd xi <3025613752@qq.com>
+Homepage: https://github.com/gfdgd-xi/dclc-kernel
+Architecture: arm64
+Severity: serious
+Certainty: possible
+Check: binaries
+Type: binary, udeb
+Priority: optional
+Depends: linux-headers-$VERSION-arm64-gfdgdxi-desktop-hwe, linux-image-$VERSION-arm64-gfdgdxi-desktop-hwe
+Section: utils
+Installed-Size: 0
+Description: 内核 $VERSION
+EOF
+    if [[ ! -d deb-$MAINVERSION ]]; then
+        mkdir -pv deb-$MAINVERSION/DEBIAN
+    fi
+    cat > deb-$MAINVERSION/DEBIAN/control <<EOF
+Package: linux-kernel-gxde-gfdgdxi-$1-hwe
+Version: $VERSION
+Maintainer: gfdgd xi <3025613752@qq.com>
+Homepage: https://github.com/gfdgd-xi/dclc-kernel
+Architecture: arm64
+Severity: serious
+Certainty: possible
+Check: binaries
+Type: binary, udeb
+Priority: optional
+Depends: linux-headers-$VERSION-arm64-gfdgdxi-desktop-hwe, linux-image-$VERSION-arm64-gfdgdxi-desktop-hwe
+Section: utils
+Installed-Size: 0
+Description: 内核 HWE $VERSION
+EOF
+    dpkg-deb -Z xz -b deb linux-kernel-gxde-gfdgdxi-hwe_${VERSION}_arm64.deb
+    dpkg-deb -Z xz -b deb-$MAINVERSION linux-kernel-gxde-gfdgdxi-$1-hwe_${VERSION}_arm64.deb
+    cd ../../gxde-linux-kernel
+    #bash ./repack-zstd --scan .
+    #./build.py
+    ./move-letter-path.py ../dclc-kernel/$SHOWVERSION/*.deb
+    ./move-letter-path.py ../dclc-kernel/head/*.deb
+    ./move-letter-path.py /tmp/deb-result/*.deb
+    touch l/linux-headers-$VERSION-arm64-gfdgdxi-desktop-hwe/lock
+    rm -rfv l/linux-libc-dev/
+    git add .
+    #git pull
+    git config --global user.email 3025613752@qq.com
+    git config --global user.name gfdgd-xi
+    git commit -m 提交$VERSION
+    git push
+else
+    # move deb packages to artifact dir
+    cd ..
+    mkdir "artifact"
+    #cp ./*.deb artifact/
+    rm -rfv "${GITHUB_WORKSPACE}/linux-$VERSION*"
+    git clone https://gfdgd-xi:$PASSWORD@github.com/gfdgd-xi/gxde-linux-kernel --depth=1
+    #cd dclc-kernel
+    mkdir dclc-kernel
+    mkdir dclc-kernel/$VERSION
+    mkdir dclc-kernel/head
+    rm -rfv *dbg*.deb
+    mv ./*.deb dclc-kernel/$VERSION
+    cd dclc-kernel/$VERSION
+    cd ..
+    cd head
+    mkdir deb/DEBIAN -vp
+    cat > deb/DEBIAN/control <<EOF
 Package: linux-kernel-gxde-gfdgdxi
 Version: $VERSION
 Maintainer: gfdgd xi <3025613752@qq.com>
@@ -94,7 +183,10 @@ Section: utils
 Installed-Size: 0
 Description: 内核（虚包）
 EOF
-cat > deb-$MAINVERSION/DEBIAN/control <<EOF
+    if [[ ! -d deb-$MAINVERSION ]]; then
+        mkdir -pv deb-$MAINVERSION/DEBIAN
+    fi
+    cat > deb-$MAINVERSION/DEBIAN/control <<EOF
 Package: linux-kernel-gxde-gfdgdxi-$1
 Version: $VERSION
 Maintainer: gfdgd xi <3025613752@qq.com>
@@ -110,17 +202,20 @@ Section: utils
 Installed-Size: 0
 Description: 内核（虚包）
 EOF
-dpkg -b deb linux-kernel-gxde-gfdgdxi_${VERSION}_arm64.deb
-dpkg -b deb-$MAINVERSION linux-kernel-gxde-gfdgdxi-${1}_${VERSION}_arm64.deb
-cd ../../gxde-linux-kernel
-#bash ./repack-zstd --scan .
-#./build.py
-./move-letter-path.py ../dclc-kernel/$VERSION-aarch64/*.deb
-./move-letter-path.py ../dclc-kernel/head/*.deb
-touch l/linux-headers-$VERSION-arm64-gfdgdxi-desktop/lock
-rm -rfv l/linux-libc-dev/
-git add .
-git config --global user.email 3025613752@qq.com
-git config --global user.name gfdgd-xi
-git commit -m 提交$VERSION
-git push
+    dpkg-deb -Z xz -b deb linux-kernel-gxde-gfdgdxi_${VERSION}_arm64.deb
+    dpkg-deb -Z xz -b deb-$MAINVERSION linux-kernel-gxde-gfdgdxi-${1}_${VERSION}_arm64.deb
+    cd ../../gxde-linux-kernel
+    #bash ./repack-zstd --scan .
+    #./build.py
+    ./move-letter-path.py ../dclc-kernel/$VERSION/*.deb
+    ./move-letter-path.py ../dclc-kernel/head/*.deb
+    ./move-letter-path.py /tmp/deb-result/*.deb
+    rm -rfv l/linux-libc-dev/
+    touch l/linux-headers-$VERSION-arm64-gfdgdxi-desktop/lock
+    git add .
+    #git pull
+    git config --global user.email 3025613752@qq.com
+    git config --global user.name gfdgd-xi
+    git commit -m 提交$VERSION
+    git push
+fi
