86759a1bc5accfb531ee9fafc6dc2b509781d9e7
gfdgd_xi <3025613752@qq.com>
2024-05-18 09:54:41 +0800

    修复在没有安装aptss时安装内核会提示错误1

--- shell/kernel-installer-update-template.sh
+++ shell/kernel-installer-update-template.sh
@@ -2,8 +2,7 @@
 set -e
 rm /tmp/gxde-kernel-manager-installer-status -f
 aptPath="apt"
-which aptss > /dev/null
-if [[ $? == 0 ]]; then
+if [[ -f "/usr/bin/aptss" ]]; then
     # 如果 aptss 存在，则使用 aptss
     aptPath="aptss"
 fi
