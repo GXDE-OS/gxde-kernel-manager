c45baaa6aa391ca01fb889671554e154f85b48db
gfdgd_xi <3025613752@qq.com>
2024-07-19 13:34:02 +0800

    a

--- build_action-loong64-4k-pagesize.sh
+++ build_action-loong64-4k-pagesize.sh
@@ -27,6 +27,7 @@ fi
 if [[ $result != 0 ]]; then
     # 临时关闭检测
     #exit
+    echo a
 fi
 # install dep
 sudo apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev aria2
