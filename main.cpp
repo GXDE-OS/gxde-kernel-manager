504938fd8c10c77b222d476f159c270f47bc470d
gfdgd-xi <3025613752@qq.com>
2025-09-21 11:39:44 +0800

    阻止小小电脑用户启动内核管理器
    Signed-off-by: gfdgd-xi <3025613752@qq.com>

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include <QDir>
 #include <QMessageBox>
 #include <QObject>
+#include <QDBusInterface>
+
+#define CHROOTCHECKDESTINATION "com.gxde.daemon.system.info"
+#define CHROOTCHECKPATH "/com/gxde/daemon/system/info"
+#define CHROOTCHECKINTERFACE "com.gxde.daemon.system.info"
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +23,16 @@ int main(int argc, char *argv[])
     }
     a.installTranslator(&translator);
 #ifdef __linux__
+    // 判断是否在 Chroot 下运行，如果是则不显示电源按钮
+    QDBusMessage checkChrootDBus = QDBusMessage::createMethodCall(CHROOTCHECKDESTINATION,
+                                                                  CHROOTCHECKPATH,
+                                                                  CHROOTCHECKINTERFACE,
+                                                                  "IsInChroot");
+    QDBusMessage res = QDBusConnection::sessionBus().call(checkChrootDBus);
+    if (res.arguments().at(0).toBool()) {
+        QMessageBox::critical(NULL, QObject::tr("Error"), QObject::tr("It unsupport tiny computer or termux"));
+        exit(1);
+    }
     // 风险提示
     if(!QFile::exists(QDir::homePath() + "/.config/gxde-kernel-manager/read_already")) {
         QMessageBox::warning(NULL, QObject::tr("Waring"), QObject::tr("It is danger because it may make your system unboot.\nPress 'OK' to countinue"));
